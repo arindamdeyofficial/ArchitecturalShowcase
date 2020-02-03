5e8e66c32aa3ba2510f6b5aa21874f7bf646bdee
Arindam Dey <biplabhome@gmail.com>
2020-02-03 18:14:08 +0530

    bin obj .vs removed and .gitignore added

--- ProductCatalogueUi/prdUi/src/app/app.component.ts
+++ ProductCatalogueUi/prdUi/src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ProductCatalogue';
+  columnDefs = [
+    {headerName: 'Title', field: 'title', editable: true},
+    {headerName: 'Cost', field: 'cost', editable: true},
+    {headerName: 'Quantity', field: 'quantity', editable: true},
+    {headerName: 'TotalCost', field: 'totalCost', editable: true}
+];
+
+rowData = [];
+
+  ngOnInit() {
+    fetch('http://localhost:5000/api/ProductCatalogue/SearchProduct')
+      .then(result => result.json())
+      .then(rowData => this.rowData = rowData);
+  }
 }
